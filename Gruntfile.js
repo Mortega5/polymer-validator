be424bfb58e3e323f62262a832d1290787922434
Miguel Ortega Moreno <mortega@conwet.com>
2016-10-31 13:37:04 +0100

    removed bower_components

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,15 +22,27 @@ module.exports = function(grunt) {
           'validator.min.js': ['validator.js']
         }
       }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'validator.min.css': ['validator.css']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('test', ['mocha']);
-  grunt.registerTask('dest',['mocha','uglify']);
+  grunt.registerTask('dest',['mocha','uglify','cssmin']);
   grunt.registerTask('default', ['test']);
 
 };
