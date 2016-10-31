9bacb97037f55ece09a1bbecd65b0199d4a05f3c
Miguel Ortega Moreno <mortega@conwet.com>
2016-10-31 13:10:51 +0100

    Updated readme

--- readme.md
+++ readme.md
@@ -9,21 +9,41 @@ La función `validateFn(value,contents)` recibe dos argumento:
   * `contents`: Es una lista con los subtipos que se definen para los elementos
   internos. Es utilizado para, por ejemplo, en tipos como array definir de que
   tipo tienen que ser los valores que encontramos dentro del array.
+Ejemplo de tipo
 
-  ```javasript
-    // Este array es de tipo array y el contenido es de tipo Number es decir
-    // Array.Number sera pasado como tipo lo que dara como resultado que contents
-    // sea igual a `[Number]``
-    var array = [1,2,3]
+```javascript
+{
+  "String":{
+    "type":"simpleType",
+    "validateFn": function(value, contents, cb){
+      var result = typeof(value) == "string";
+      if (cb){
+        cb(result);
+      }
+      return result;
+    }
+  }
+}
+```
 
-  ```
-  Permite anidamientos de tipos que se van pasando como argumento
+Ejemplo
 
-  ``` javascript
-    // En este caso el tipo es Array.Array.Number por lo que contents
-    // recibirá en primera instancia [Array, Number]
-    var array = [[1,2,3],[4,5,6],[7,8,9]]
-  ```
+```javasript
+
+// Este array es de tipo array y el contenido es de tipo Number es decir
+// Array.Number sera pasado como tipo lo que dara como resultado que contents
+// sea igual a `[Number]`
+
+var array = [1,2,3]
+
+```
+Permite anidamientos de tipos que se van pasando como argumento
+
+``` javascript
+// En este caso el tipo es Array.Array.Number por lo que contents
+// recibirá en primera instancia [Array, Number]
+var array = [[1,2,3],[4,5,6],[7,8,9]]
+```
 
   ## Uso
 
@@ -34,16 +54,57 @@ La función `validateFn(value,contents)` recibe dos argumento:
   ```
   Después podremos invocar a Validator para realizar diferentes funciones.
 
-  ### `Validator.validate(type, value)`
+  ### `Validator.checkType(type, value, cb)`
+
+  Valida valor en un determinado typo.
 
     * `type` es el tipo con el que queremos comprar `value`
     * `value` es el dato que vamos a comprobar si es es del `type` indicado
+    * `cb` callback ejecutado una vez se termine de comprobar el tipo
 
-    `return` si value es del typo indicado
+  ### `Validator.validate(list, cb, conf)`
+
+  Valida un elemento polymer o varios si se le pasa una lista de elementos.
+
+    * `list` component o lista de componentes que se quieren validar
+    * `cb` funcion que se ejecutara tras terminar la validación de cada componente que se pase. Recibe un objeto como parametro:
+
+    ```javascript
+    {
+      passed:{
+        component:google-geocoding#geo,
+        passed: true,
+        phase: "definition", // Can be definition or change
+        property:"data",
+        type: "Array",
+        value: []
+      },
+      errors:{
+        component:google-geocoding#geo,
+        passed: true,
+        phase: "definition",
+        property:"location",
+        type: "Coordinates",
+        value: 'test'
+      },
+    }
+    ```
+    * `conf` Configuración del validador.
+      {
+        executable: function que se ejecuta cuando se valida el componente, si es una lista de componente se puede pasar una lista de funciones a executar en el mismo orden que se reciben los componentes
+        timeout: tiempo de espera hasta que el componente termine
+      }
+  ### `Validator.extendTypes(custom_type)`
+    Permite extender y cambiar las propiedades que se validan asi como sus funciones
+
+    * `custom_type` Objeto con las nuevas propiedades a definir. Deben tener el formato adecuado:{type:'simpleType or complexType', validateFn: function(value, contents, cb){//function que valide}}
+
+    `value` es el valor que encuentra de entrada a la hora de Validator
+    `contents` es la lista de propiedades anidadas, para validar propiedades del estilo Array.Number
+    `cb` function de callback donde se le pasa el booleano con el resultado de la comprobación
 
   ### `Validator._type`
     Lista de los tipos que es capaz de manejar con sus funciones y sus subtipos
-
   ## Test
 
   Antes de pasar los test es necesario instalar las dependencias
--- validator.js
+++ validator.js
@@ -1,5 +1,6 @@
 
 (function(window){
+
   var types = {
     "String":{
       "type":"simpleType",
@@ -114,8 +115,8 @@
         var result = element instanceof HTMLElement;
 
         if (result && contents && contents.length > 0){
-            result = element.tagName === contents[0].toUpperCase() && result;
-          }
+          result = element.tagName === contents[0].toUpperCase() && result;
+        }
 
         if (cb){
           cb(result);
@@ -132,13 +133,13 @@
           if (!coordinates.longitude && !coordinates.longitude){
             result = true;
           } else {
-          // coordinates are numbers
-          result = types.Number.validateFn(coordinates.longitude) && types.Number.validateFn(coordinates.latitude);
-          // check latitude is between -90 and +90
-          result = coordinates.latitude < 90 && coordinates.latitude > -90 && result;
-          // check longitude is between -180 and +180
-          result = coordinates.longitude < 180 && coordinates.longitude > -180 && result;
-        }
+            // coordinates are numbers
+            result = types.Number.validateFn(coordinates.longitude) && types.Number.validateFn(coordinates.latitude);
+            // check latitude is between -90 and +90
+            result = coordinates.latitude < 90 && coordinates.latitude > -90 && result;
+            // check longitude is between -180 and +180
+            result = coordinates.longitude < 180 && coordinates.longitude > -180 && result;
+          }
         }
         if (cb){
           cb(result);
@@ -193,6 +194,64 @@
     Validator = function(){
       this._types = types;
     };
+
+    /**
+     * Validator.prototype._createReport - Create a html report
+     *
+     * @param  {String} name   Name of the component validated
+     * @param  {Array} passed List of test passed
+     * @param  {Array} errors List of test with erros.
+     */
+    Validator.prototype._createReport = function(name, passed, errors){
+      var body = document.querySelector('body');
+      body.setAttribute('ready',true);
+      // Component header
+      var component_name = document.createElement('H1');
+      component_name.innerHTML = name;
+      component_name.className = 'name';
+      // Error report
+      var div_errors = document.createElement('UL');
+      var h1_errors = document.createElement('H2');
+      h1_errors.innerHTML = "Errors with output type";
+      h1_errors.className= 'title';
+      div_errors.appendChild(h1_errors);
+      div_errors.className = 'validator fail';
+
+      // Success report
+      var div_passed = document.createElement('UL');
+      var h1_passed = document.createElement('H2');
+      h1_passed.innerHTML = "Success with output type";
+      h1_passed.className = 'title';
+      div_passed.appendChild(h1_passed);
+      div_passed.className = 'validator success';
+
+      for (var i=0;i<errors.length;i++){
+        var div_err = document.createElement('LI');
+        div_err.innerHTML = '<span class="property">' + errors[i].property + '</span> should be type of ' + errors[i].type + ' in ' + errors[i].phase + ' phase';
+        div_err.className = 'validator item';
+        div_errors.appendChild(div_err);
+      }
+      for (var j=0;j<passed.length;j++){
+        var div_pas = document.createElement('LI');
+        div_pas.innerHTML = '<span class="property">' + passed[j].property + '</span> is type  ' + passed[j].type + ' in ' + passed[j].phase + ' phase';
+        div_pas.className = 'validator item';
+        div_passed.appendChild(div_pas);
+      }
+      body.appendChild(component_name);
+      if (errors.length !==0){
+        body.appendChild(div_errors);
+      }
+      if (errors.succes !==0){
+        body.appendChild(div_passed);
+      }
+    };
+
+    /**
+    * Validator.prototype.checkType - Create a html reportCheck if  `value` is of the type `type`
+    * @param {String} Type of the object which will be tested
+    * @param {value} Value Value will be checkd and which be equals to type
+    * @param {function} cb Callback before check if type and type of value are equals
+    */
     Validator.prototype.checkType = function(type,value,cb){
       var content;
       var checkContent = type.split('.');
@@ -207,9 +266,20 @@
         this._types[type].validateFn(value,content,cb);
       }
     };
+    /**
+      * Validator.prototype.validate - Validate a Polymer element or an array of Polymer Elements
+      * @param {HTMLElement} List Element or elements which will be validated. You can test multiple element in a array
+      * @param {Function} cb Callback called after an element is tested.
+      * @param {Object} conf Configuration for validate an element. \
+        {
+          executable: function will be executed when a component is tested,
+          timeout: time to wait a component finnish.
+        }
+    */
     Validator.prototype.validate = function(list, cb, conf){
       // If conf is a executable function we put it in an object
-      if (typeof(conf) === 'function'){
+      conf = conf || {};
+      if (conf instanceof  Function){
         conf = {executable:conf};
       }
       // single component
@@ -217,10 +287,27 @@
         this.validateComponent(list,cb, conf);
       } else if (list instanceof Array){
         for (var i=0;i<list.length;i++){
-          this.validateComponent(list[i],cb, conf[list[i]]);
+          var custom_config = {};
+          // If multiple functions
+          if (conf.executable instanceof Array && i < conf.executable.length ) {
+            custom_config = {executable:conf.executable[i], timeout:conf.timeout};
+          } else if (conf.executable instanceof Function){
+            custom_config = conf;
+          }
+          this.validateComponent(list[i],cb, custom_config);
         }
       }
     };
+    /**
+      * Validator.prototype.validateComponent - Validate a Polymer element or an array of Polymer Elements
+      * @param {HTMLElement} component Element will be validated.
+      * @param {Function} cb Callback called after an element is tested.
+      * @param {Object} conf Configuration for validate an element. \
+        {
+          executable: function will be executed when a component is tested,
+          timeout: time to wait a component finnish.
+        }
+    */
     Validator.prototype.validateComponent = function(component,cb, conf){
       var errors = [];
       var passed = [];
@@ -263,55 +350,25 @@
       }
       // Execute a code after load the component
       if (conf.executable) conf.executable();
-
-      var createReport = function(name, passed, errors){
-        var body = document.querySelector('body');
-        body.setAttribute('ready',true);
-        // Component header
-        var component_name = document.createElement('H1');
-        component_name.innerHTML = name;
-        component_name.className = 'name';
-        // Error report
-        var div_errors = document.createElement('UL');
-        var h1_errors = document.createElement('H2');
-        h1_errors.innerHTML = "Errors with output type";
-        h1_errors.className= 'title';
-        div_errors.appendChild(h1_errors);
-        div_errors.className = 'validator fail';
-
-        // Success report
-        var div_passed = document.createElement('UL');
-        var h1_passed = document.createElement('H2');
-        h1_passed.innerHTML = "Success with output type";
-        h1_passed.className = 'title';
-        div_passed.appendChild(h1_passed);
-        div_passed.className = 'validator success';
-
-        for (var i=0;i<errors.length;i++){
-          var div_err = document.createElement('LI');
-          div_err.innerHTML = '<span class="property">' + errors[i].property + '</span> should be type of ' + errors[i].type + ' in ' + errors[i].phase + ' phase';
-          div_err.className = 'validator item';
-          div_errors.appendChild(div_err);
-        }
-        for (var j=0;j<passed.length;j++){
-          var div_pas = document.createElement('LI');
-          div_pas.innerHTML = '<span class="property">' + passed[j].property + '</span> is type  ' + passed[j].type + ' in ' + passed[j].phase + ' phase';
-          div_pas.className = 'validator item';
-          div_passed.appendChild(div_pas);
-        }
-        body.appendChild(component_name);
-        if (errors.length !==0){
-          body.appendChild(div_errors);
-        }
-        if (errors.succes !==0){
-          body.appendChild(div_passed);
-        }
-      };
       setTimeout(function(){
         cb({passed: passed, errors:errors});
-        createReport(component.tagName, passed,errors);
+        console.log(this);
+        Validator._createReport(component.tagName, passed,errors);
 
       }.bind(this),conf.timeout|| 3000);
+    };
+
+    Validator.prototype.extendTypes = function(custom_types){
+      for (var type in custom_types){
+        if (custom_types.hasOwnProperty(type)){
+          if (custom_types[type].type && custom_types[type].validateFn){
+            this._types[type] = custom_types[type];
+          } else {
+            console.error('new types must have type and validateFn properties');
+            return;
+          }
+        }
+      }
     };
     return new Validator();
   })(types);
